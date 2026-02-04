424bdf7990b472a67fa1723d2e212550e9ede806
robiuljoy <robiuljoy91@gmail.com>
2026-02-05 00:54:36 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 
 const port = process.env.PORT || 3000;
@@ -9,9 +10,38 @@ app.use(cors());
 
 app.use(express.json());
 
+// Mongodb connection
+const uri =
+  "mongodb+srv://scholarServer:8QN3E8xktVazUUHl@cluster0.hl8gbtt.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("simple crud server is running");
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Simple Crud server is running on Port ${port}`);
